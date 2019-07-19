b3627736169d6da32ebfac7ca083ece4ce53dee7
Heather deManbey <h.demanbey@gmail.com>
2019-07-19 11:34:32 -0400

    finalized google auth sign in and button styling

--- src/App.js
+++ src/App.js
@@ -5,27 +5,27 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import Header from './components/header/header.component';
 import { auth } from './firebase/firebase.utils';
 
 class App extends React.Component {
   constructor() {
     super();
-    
+
     this.state = {
       currentUser: null
-    }
+    };
   }
 
-  unsubscribeFromAuth = null
+  unsubscribeFromAuth = null;
 
   componentDidMount() {
     this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user })
+      this.setState({ currentUser: user });
 
       console.log(user);
-    })
+    });
   }
 
   componentWillUnmount() {
@@ -35,11 +35,11 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Header />
+        <Header currentUser={this.state.currentUser} />
         <Switch>
           <Route exact path='/' component={HomePage} />
-          <Route exact path='/shop' component={ShopPage} />
-          <Route exact path='/signin' component={SignInAndSignUpPage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route path='/signin' component={SignInAndSignUpPage} />
         </Switch>
       </div>
     );

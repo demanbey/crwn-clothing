dd06585edfa1ba4b9e643138a7ee002a4a919a69
Heather deManbey <h.demanbey@gmail.com>
2019-07-29 15:25:08 -0400

    sign-in/sign out fix

--- src/App.js
+++ src/App.js
@@ -12,18 +12,10 @@ import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions';
 
 class App extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      currentUser: null
-    };
-  }
-
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    const {setCurrentUser} = this.props;
+    const { setCurrentUser } = this.props;
 
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
@@ -36,8 +28,8 @@ class App extends React.Component {
           });
         });
       }
-      
-      setCurrentUser({ userAuth });
+
+      setCurrentUser(userAuth);
     });
   }
 
@@ -61,6 +53,9 @@ class App extends React.Component {
 
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
-})
+});
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(
+  null,
+  mapDispatchToProps
+)(App);
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,7 +3,6 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 
-
 const config = {
     apiKey: "AIzaSyC46WAWFxU6aiep0P6KtdrCZdN5Ss3seUs",
     authDomain: "crwn-clothing-db-5c4fb.firebaseapp.com",
@@ -14,44 +13,38 @@ const config = {
     appId: "1:827172977256:web:d5223745a26402d8"
 };
 
-export const createUserProfileDocument = async (userAuth, additionalData) => {
-    if (!userAuth) return; 
-
-    const userRef = firestore.doc(`users/${userAuth.uid}`);
-
-    const snapShot = await userRef.get();
-
-    if(!snapShot.exists) {
-        const { displayName, email } = userAuth;
-        const createdAt = new Date();
-
-        try {
-            await userRef.set({
-                displayName,
-                createdAt, 
-                email, 
-                ...additionalData
-            })
-        } catch (error) {
-            console.log('error creating user', error.message);
-        }
-    }
-
-    return userRef;
-}
-
 firebase.initializeApp(config);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
+};
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: 'select_account' });
-export const signInWithGoogle = () => auth.signInWithPopup(provider)
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
-
-
-
-
-
